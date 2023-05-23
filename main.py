f21d3413b23cd8f32de627ba81d21ba5ddd5be08
antariksh247 <77575313+antariksh247@users.noreply.github.com>
2023-05-23 09:57:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-import streamlit
-streamlit.header('vijya bhau cha menu')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+import streamlit as st
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+def login():
+    st.title("Login Page")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    login_button = st.button("Login")
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
+    if login_button:
+        if username == "admin" and password == "password":
+            st.success("Logged in successfully!")
+            # Add your code for the logged-in page or redirect to another page
+        else:
+            st.error("Invalid username or password.")
 
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-streamlit.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-
-# Display the table on the page.
-
+login()
