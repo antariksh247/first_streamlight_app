9325ede68de2660ed4e14119f77247d836b8a75e
antariksh247 <77575313+antariksh247@users.noreply.github.com>
2023-05-23 10:45:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
-# Using st.text()
-st.text("This is a normal text using st.text()")
+def login():
+    st.title("Login Page")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    login_button = st.button("Login")
 
-# Using st.write()
-st.write("This is a normal text using st.write()")
+    if login_button:
+        if username == "admin" and password == "password":
+            st.success("Logged in successfully!")
+            # Add your code for the logged-in page or redirect to another page
+        else:
+            st.error("Invalid username or password.")
+
+login()
